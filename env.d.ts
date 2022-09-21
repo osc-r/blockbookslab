f9677f31cac5bc72890b3f60a87d31f89544842b
Chanatip R <idontwanttoshare@mymail.com>
2022-09-21 21:59:30 +0700

    add: env file

---
+++ env.d.ts
@@ -0,0 +1,14 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_NOTIFICATION_CHANNEL_ADDR: string;
+      NEXT_PUBLIC_NOTIFICATION_ENV: string;
+      NEXT_PUBLIC_NOTIFICATION_APP_NAME: string;
+      NEXT_PUBLIC_API_ENDPOINT: string;
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
+const ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  styledComponents: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
