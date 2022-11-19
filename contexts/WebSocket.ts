2e78bee46eac7ed948ed0fe14aef7528c2f5b2f6
Chanatip R <chanatip.rassameecharerntham@scb.co.th>
2022-11-20 04:10:35 +0700

    fix: websocket connection

--- contexts/WebSocket.ts
+++ contexts/WebSocket.ts
@@ -8,35 +8,39 @@ const WebSocket = () => {
   const walletState = useSelector((state: RootState) => state.app.wallets);
 
   useEffect(() => {
-    console.log({ walletState });
-    const socket = io(process.env.NEXT_PUBLIC_API_ENDPOINT);
+    let socket: any = null;
 
-    socket.on("connect", () => {
-      console.log(socket.id);
-    });
+    if (typeof window !== "undefined") {
+      console.log({ walletState });
+      socket = io(process.env.NEXT_PUBLIC_API_ENDPOINT);
 
-    socket.on(
-      "SYNCED_TRANSACTION_JOB",
-      (data: { success: string; address: string }) => {
-        const isNotify = walletState.find((i) => i.address === data.address);
-        console.log({ data, isNotify });
-        if (isNotify) {
-          message.destroy();
-          setTimeout(() => {
-            message.success("Wallet has been completely synced");
-          }, 700);
+      socket.on("connect", () => {
+        console.log(socket.id);
+      });
+
+      socket.on(
+        "SYNCED_TRANSACTION_JOB",
+        (data: { success: string; address: string }) => {
+          const isNotify = walletState.find((i) => i.address === data.address);
+          console.log({ data, isNotify });
+          if (isNotify) {
+            message.destroy();
+            setTimeout(() => {
+              message.success("Wallet has been completely synced");
+            }, 700);
+          }
         }
-      }
-    );
+      );
 
-    socket.on("disconnect", () => {
-      console.log(socket.id);
-    });
+      socket.on("disconnect", () => {
+        console.log(socket.id);
+      });
+    }
 
     return () => {
       socket.disconnect();
     };
-  }, [walletState]);
+  }, [walletState, window]);
 
   return null;
 };
